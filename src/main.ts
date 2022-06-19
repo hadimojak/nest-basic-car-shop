2e90231f2926cea71422277069c62667c4fca959
hadimojak <hadimojak@gmail.com>
2022-06-19 17:23:44 +0430

    inviroent variable created

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-const cookiesession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookiesession({
-      keys: ['asd87s8'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      //this option secure the body of request and wipe out all other property
-      whitelist: true,
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
